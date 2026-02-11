31bb53df391b1d771794ac17c3f65900cebf1d0d
webpitrial <webpitrial@gmail.com>
2026-02-11 14:21:32 +0530

    Fix Build Errors

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
